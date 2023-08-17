3d032d34af1e1d0ead4e884aae47cdcb79d1abcf
Mackensie Jack <kensiejack1@gmail.com>
2023-08-17 15:27:50 -0400

    initialize cheatsheet

---
+++ app/cheatsheet/page.jsx
@@ -0,0 +1,9 @@
+const CheatSheet = () => {
+  return (
+    <main className="p-10 rounded text-slate-700 w-5/6 bg-indigo-50/75">
+      <p>This is the cheatsheet!</p>
+    </main>
+  );
+};
+
+export default CheatSheet;
